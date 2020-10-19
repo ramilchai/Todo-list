a9415f0688d14ba0ebce46cd38c2d0fd1dfd89d4
Ramil <ramil.ming@gmail.com>
2020-10-18 20:16:42 -0700

    First Commit

---
+++ app.js
@@ -0,0 +1,13 @@
+//Selectors 
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+//Event Listeners 
+todoButton.addEventListener("click", addTodo);
+//Funstions
+
+function addTodo(event){
+    // event.preventDefault();
+    // console.log("Hello");
+}
