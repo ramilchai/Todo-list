8d42fa74bce9c1a12c8aa001cd593f5bc50c97ec
Ramil <ramil.ming@gmail.com>
2020-10-18 20:48:04 -0700

    create adding list

--- app.js
+++ app.js
@@ -8,6 +8,40 @@ todoButton.addEventListener("click", addTodo);
 //Funstions
 
 function addTodo(event){
-    // event.preventDefault();
-    // console.log("Hello");
+    
+    event.preventDefault();
+    //What we want is:
+    //  <div class="todo">
+    //          <li></li>
+    //          <button></button>
+    //          <button></button>
+    //  </div>
+
+    //Create DIV
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+
+    //Create LI
+    const newTodo = document.createElement("li");
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo);
+
+    //Create Mark BUTTON 
+    const completedButton = document.createElement("button");
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+   
+    //Create trash BUTTON 
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+
+    //APPEND to LIST
+    todoList.appendChild(todoDiv);
+
+    //Clear Todo input
+    todoInput.value = "";
 }
